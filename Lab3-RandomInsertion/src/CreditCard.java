8cab3201ecf8e3031ea00c0d350e74c28048f292
Yang Pan <ypan01@hotmail.com>
2015-10-15 10:17:37 -0700

    Simple setup of classes to be used in driver

---
+++ Lab3-RandomInsertion/src/CreditCard.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yangpan
+ */
+public class CreditCard {
+    private String number;
+    private String type;
+    private int creditLimit;
+    private double currentBalance;
+    
+    public CreditCard() {
+        
+    }
+}
---
+++ Lab3-RandomInsertion/src/Customer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yangpan
+ */
+public class Customer {
+    private int SSN;
+    private int customerId;
+    private String name;
+    private String address;
+    private String phone;
+    
+    public Customer() {
+        
+    }
+}
